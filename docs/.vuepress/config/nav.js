27c2b46bbce4dba1dad409c3f8e003479393c12e
zhangdongdong <2513022260@qq.com>
2021-07-01 00:01:05 +0800

    📃 docs: 去除子菜单

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -4,12 +4,12 @@ module.exports = [
   { 
     text: '技术',
     link: '/technology/',
-    items: [
-      { text: '技术文档', link: '/pages/9a7ee40fc232253e/' },
-      { text: 'GitHub技巧', link: '/pages/4c778760be26d8b3/' },
-      { text: 'Nodejs', link: '/pages/117708e0af7f0bd9/' },
-      { text: '博客搭建', link: '/pages/41f87d890d0a02af/' },
-    ]
+    // items: [
+    //   { text: '技术文档', link: '/pages/9a7ee40fc232253e/' },
+    //   { text: 'GitHub技巧', link: '/pages/4c778760be26d8b3/' },
+    //   { text: 'Nodejs', link: '/pages/117708e0af7f0bd9/' },
+    //   { text: '博客搭建', link: '/pages/41f87d890d0a02af/' },
+    // ]
   },
   { text: '更多', link: '/more/' },
   { text: '关于', link: '/about/' },
