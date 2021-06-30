d652766e1182e009ec0d5249e232eec3a0557b97
zhangdongdong <zhangdongdong@qjdchina.com>
2021-06-30 18:32:50 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  title: '个人博客',
+  description: '个人博客，用于技术博客、技术分享、文章整理。',
+  theme: 'vdoing',
+  themeConfig: {
+    logo: '',
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/guide/' },
+      { text: 'External', link: 'https://google.com' },
+    ]
+  }
+}
---
+++ docs/.vuepress/config/nav.js
@@ -0,0 +1,7 @@
+module.exports = [
+  { text: '首页', link: '/' },
+  { text: '分类', link: '/categories' },
+  { text: '标签', link: '/tags' },
+  { text: '关于', link: '/about' },
+  { text: 'githup', link: 'https://github.com/2513022260/blog' },
+]
