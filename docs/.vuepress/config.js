ad5d588a6a258377fb963ad24c8eb045105e45b7
zhangdongdong <zhangdongdong@qjdchina.com>
2021-07-01 10:42:45 +0800

    ✨ feat(config.js): 部署：添加base配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,6 +5,7 @@ module.exports = {
   theme: 'vdoing', // 使用npm包主题
   title: '旗木五五开',
   description: '个人博客，用于技术博客、技术分享、文章整理。',
+  base: '/blog/',
   markdown: {
     lineNumbers: true, // 代码行号
   },
