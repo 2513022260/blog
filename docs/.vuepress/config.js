074a8ca6c60557d259c13c145705fe696e24fe58
zhangdongdong <zhangdongdong@qjdchina.com>
2021-07-02 15:51:45 +0800

    ✨ feat(技术-博客搭建):
    使用VuePress搭建博客

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,9 +6,9 @@ module.exports = {
   title: '旗木五五开',
   description: '个人博客，用于技术博客、技术分享、文章整理。',
   base: '/blog/',
-  markdown: {
-    lineNumbers: true, // 代码行号
-  },
+  // markdown: {
+  //   lineNumbers: true, // 代码行号
+  // },
   head,
   themeConfig
 }
