c52498026ce2d4b791ca02be62d713f96fb3831e
zhangdongdong <2513022260@qq.com>
2022-02-28 23:59:18 +0800

    ✨ feat(promise): then 即 链式调用

--- source/Promise.js
+++ source/Promise.js
@@ -6,22 +6,99 @@ class myPromise {
     this.status = PENDING
     this.value = null
     this.reason = null
-    fn(this.resolve.bind(this), this.reject.bind(this))
+    this.onFulfilledCallbacks = []  // 保存成功回调
+    this.onRejectedCallbacks = []  // 保存失败回调
+    try {
+      fn(this.resolve.bind(this), this.reject.bind(this))
+    } catch (error) {
+      this.reject(error) 
+    }
   }
   resolve(value) {
     if (this.status === PENDING) {
-      this.status = FULFILLED
-      this.value = value
+      setTimeout(() => {
+        this.status = FULFILLED
+        this.value = value
+        this.onFulfilledCallbacks.forEach(callback => {
+          callback(value)
+        })
+      })
     }
   }
   reject(reason) {
     if (this.status === PENDING) {
-      this.status = REJECTED
-      this.reason = reason
+      setTimeout(() => {
+        this.status = REJECTED
+        this.reason = reason
+        this.onRejectedCallbacks.forEach(callback => {
+          callback(reason)
+        })
+      })
     }
+  }
+  then(onFulfilled, onRejected) {
+    onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : value => value
+    onRejected = typeof onRejected === 'function' ? onRejected : reason => { throw reason }
+    // 新增Promise实例并返回
+    const promise2 = new myPromise((resolve, reject) => {
+      if (this.status === FULFILLED) {
+        setTimeout(() => {
+          try {
+            const x = onFulfilled(this.value)
+            resolvePromise(promise2, x, resolve, reject) 
+          } catch (error) {
+            reject(error)
+          }
+        })
+      } else if (this.status === REJECTED) {
+        setTimeout(() => {
+          try {
+            const x = onRejected(this.reason)
+            resolvePromise(promise2, x, resolve, reject)
+          } catch (error) {
+            reject(error)
+          }
+        })
+      } else if (this.status === PENDING) {
+        this.onFulfilledCallbacks.push(() => {
+          setTimeout(() => {
+            try {
+              const x = onFulfilled(this.value)
+              resolvePromise(promise2, x, resolve, reject) 
+            } catch (error) {
+              reject(error)
+            }
+          })
+        })
+        this.onRejectedCallbacks.push(() => {
+          setTimeout(() => {
+            try {
+              const x = onRejected(this.reason)
+              resolvePromise(promise2, x, resolve, reject)
+            } catch (error) {
+              reject(error)
+            }
+          })
+        })
+      }
+    })
+    return promise2
   }
 }
 
+function resolvePromise(promise2, x, resolve, reject) {}
+
+console.log(1)
 const p = new myPromise((resolve, reject) => {
-  resolve(2222222)
+  setTimeout(() => {
+    console.log(2)
+    resolve(3)
+    console.log(4)
+  })
 })
+p.then((value) => {
+  console.log('value:' + value)
+}, (reason) => {
+  console.log(reason)
+})
+console.log(5)
